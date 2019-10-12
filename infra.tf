fec033b1a2e3a7fa7b49b00bf42d55fa0957579c
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-11 22:00:16 -0600

    Forgot to add Route53 CNAME record that validates the cert and gets it issued.

--- infra.tf
+++ infra.tf
@@ -18,6 +18,10 @@ variable "domain_tld" {
   description = "The domain name to use; this is used for creating HTTPS certificates."
 }
 
+data "aws_route53_zone" "app_dns_zone" {
+  name = "${var.domain_tld}."
+}
+
 resource "aws_s3_bucket" "serverless_bucket" {
   bucket = "${var.serverless_bucket_name}"
 }
@@ -54,6 +58,20 @@ resource "aws_acm_certificate" "app_cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "app_cert_validation_cname" {
+  name    = "${aws_acm_certificate.app_cert.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.app_cert.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.app_dns_zone.id}"
+  records = ["${aws_acm_certificate.app_cert.domain_validation_options.0.resource_record_value}"]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "app_cert" {
+  certificate_arn         = "${aws_acm_certificate.app_cert.arn}"
+  validation_record_fqdns = ["${aws_route53_record.app_cert_validation_cname.fqdn}"]
+}
+
 
 output "app_account_ak" {
   value = "${aws_iam_access_key.app.id}"
