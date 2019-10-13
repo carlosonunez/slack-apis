d9e1b5c1fab68ff7d92fa462e3cb9d9fc0debc81
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-12 18:12:24 -0600

    Fix response format
    AWS API Gateway expects responses sent to it to look like this:
    
    ```ruby
    {
      :statusCode => $A_STATUS_CODE,
      :body => $JSON_ENCODED_BODY
    }.to_json
    ```
    
    Otherwise, CloudFront sends back `HTTP/2 502 "Internal Server Error"`
    (which makes VERY LITTLE sense since (a) this is technically a `HTTP
    500` condition, (b) `HTTP 502` is actually "Bad Gateway", not "Internal
    Server Error" and (c) CloudFront was actually able to hit the origin, so
    the gateway was actually not bad).
    
    I hope this commit helps someone avoid spending 4-5 hours of their live
    parsing logs that look good, X-Ray traces that say little and
    documentation that should be better.

--- lib/slack-api/aws_helpers/api_gateway.rb
+++ lib/slack-api/aws_helpers/api_gateway.rb
@@ -3,11 +3,13 @@ require 'json'
 module SlackAPI
   module AWSHelpers
     class APIGateway
-      def self.return_200(body:)
+      def self.return_200(body:, json: nil)
+        raise "JSON should be a hash" if !json.nil? and json.class != Hash
+        default_response = { message: body }
         {
           :statusCode => 200,
-          :body => body
-        }.to_json
+          :body => json || default_response.to_json
+        }
       end
     end
   end
--- spec/unit/common/ping_spec.rb
+++ spec/unit/common/ping_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe 'Slack API Basics' do
-  it 'Should ping back' do
-    expected_response = Helpers::Unit::Aws::ApiGateway.return_200 body: "sup dawg"
+  it 'Should ping back', :unit do
+    expected_response = {
+      body: { message: 'sup dawg' }.to_json,
+      statusCode: 200
+    }
     expect(SlackAPI::Health.ping).to eq expected_response
   end
 end
