60a797a66b9cfe56caed718c4fca70df1280aa9b
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-13 00:05:15 -0600

    Use stubbing method

--- spec/unit/auth/redirect_spec.rb
+++ spec/unit/auth/redirect_spec.rb
@@ -3,10 +3,7 @@ require 'ostruct'
 
 describe "Handling fucking Slack OAuth" do
   it "Should save the OAuth code and state to DynamoDB", :unit do
-    test_put_response = OpenStruct.new
-    test_put_response.attributes = [ 'code', 'state', 'redirect_uri' ]
-    stubbed_client = Aws::DynamoDB::Client.new(stub_responses: true)
-    stubbed_client.stub_responses(:put_item, test_put_response)
+    stubbed_client = Helpers::Aws::DynamoDB.get_stubbed_client
     allow_any_instance_of(Aws::DynamoDB::Client).to receive(:new)
                                                 .and_return(stubbed_client)
     expect {SlackAPI::Auth::save_temp_code(state: 'fake_state_id',
