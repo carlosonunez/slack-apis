fc4e0862032cc02f43476447c47a009bc154ca3d
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-12 18:11:18 -0600

    Integration test teardown
    Lambda over API Gateway tests can cost (very little) money. This commit
    adds a stage for tearing this environment down.

--- scripts/execute_terraform.sh
+++ scripts/execute_terraform.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+source $(dirname "$0")/helpers/shared_secrets.sh
 TERRAFORM_STATE_S3_KEY="${TERRAFORM_STATE_S3_KEY?Please provide a S3 key to store TF state in.}"
 TERRAFORM_STATE_S3_BUCKET="${TERRAFORM_STATE_S3_BUCKET?Please provide a S3 bucket to store state in.}"
 AWS_REGION="${AWS_REGION?Please provide an AWS region.}"
@@ -17,6 +18,6 @@ terraform $action $* && \
     mkdir -p ./secrets
     for output_var in app_account_ak app_account_sk certificate_arn
     do
-      terraform output "$output_var" > "./secrets/$output_var"
+      write_secret "$(terraform output "$output_var")"
     done
   fi
