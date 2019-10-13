e63aa3dd6fbaf590bbb5dee17ea8ae0de4375713
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-13 01:07:40 -0600

    Slight refactoring

--- spec/helpers/aws/dynamodb.rb
+++ spec/helpers/aws/dynamodb.rb
@@ -3,17 +3,14 @@ require 'aws-sdk-dynamodb'
 module Helpers
   module Aws
     module DynamoDB
-      def self.get_stubbed_client
+      def self.get_stubbed_client(mocked_responses:)
         # TODO: If we start having multiple DynamoDB scenarios, fetch this info from a YAML.
-        responses = {
-          put_item: { consumed_capacity: { table_name: 'fake_table' } },
-          list_tables: [ "fake_table" ],
-          create_table: ((OpenStruct.new).table_status = "CREATING")
-        }
+        responses = mocked_responses
         stubbed_client = ::Aws::DynamoDB::Client.new(stub_responses: true)
         responses.each do |api_call, stubbed_response|
           stubbed_client.stub_responses(api_call, stubbed_response)
         end
+        stubbed_client
       end
     end
   end
