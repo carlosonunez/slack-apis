c970f0d00022de4e33e408615382a012f82d1c20
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-13 12:29:46 -0600

    Use a single library for creating stubbed AWS clients.

--- spec/unit/auth/slack_oauth_spec.rb
+++ spec/unit/auth/slack_oauth_spec.rb
@@ -27,9 +27,8 @@ MESSAGE
         list_tables: { table_names: [ "slack_api_temp_oauth_codes" ] },
         create_table: { table_description: { item_count: 0 } }
       }
-      stubbed_client = Helpers::Aws::DynamoDB.get_stubbed_client(
-        mocked_responses: responses
-      )
+      stubbed_client = Helpers::Aws.get_stubbed_client(aws_service: 'DynamoDB',
+                                                      mocked_responses: responses)
       expect(Aws::DynamoDB::Client).to receive(:new).and_return(stubbed_client)
       expect {SlackAPI::Auth::save_temp_code(state: 'fake_state_id',
                                              code: 'fake_code')}.to_not raise_error
