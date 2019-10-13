b803dfe40081a08197548fb335a4a5e8d9055e22
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-13 01:05:59 -0600

    Fix stubbed responses
    Documentation on this is really poor (or I didn't read it well enough).
    
    You don't need to construct the structs yourself (I thought you did
    since the AWS SDK calls expect structs of some kind). You can send it a
    hash of properties in the expected response that you want stubbed and
    AWS will take care of the rest.

--- spec/unit/auth/redirect_spec.rb
+++ spec/unit/auth/redirect_spec.rb
@@ -3,10 +3,16 @@ require 'ostruct'
 
 describe "Handling fucking Slack OAuth" do
   it "Should save the OAuth code and state to DynamoDB", :unit do
-    stubbed_client = Helpers::Aws::DynamoDB.get_stubbed_client
-    allow_any_instance_of(Aws::DynamoDB::Client).to receive(:new)
-                                                .and_return(stubbed_client)
+    responses = {
+      put_item: { consumed_capacity: { table_name: 'slack_api_temp_oauth_codes' } },
+      list_tables: { table_names: [ "slack_api_temp_oauth_codes" ] },
+      create_table: { table_description: { item_count: 0 } }
+    }
+    stubbed_client = Helpers::Aws::DynamoDB.get_stubbed_client(
+      mocked_responses: responses
+    )
+    expect(Aws::DynamoDB::Client).to receive(:new).and_return(stubbed_client)
     expect {SlackAPI::Auth::save_temp_code(state: 'fake_state_id',
-                                          code: 'fake_code')}.to_not raise_error
+                                           code: 'fake_code')}.to_not raise_error
   end
 end
