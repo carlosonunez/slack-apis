be3be661aa2d49cc80529d63236cc8bf47660aa7
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-13 23:17:50 -0500

    Add function mapping for begin_authentication

--- bin/slack-api.rb
+++ bin/slack-api.rb
@@ -7,6 +7,11 @@ end
 require 'slack-api'
 require 'json'
 
+# Begin the Slack OAuth flow manually.
+def begin_authentication(event: {}, context: {})
+  Slack::Auth.begin_authentication_flow(event)
+end
+
 # Slack needs a callback URI to send its code too. This is that callback.
 def handle_callback(event: {}, context: {})
   SlackAPI::Auth.handle_callback(event)
