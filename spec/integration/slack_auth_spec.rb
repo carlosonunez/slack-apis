a4a0fdb6705fa5084f2bfade2695a0911bf81540
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-13 23:17:44 -0500

    Add integration test for step 1 of the OAuth process

--- spec/integration/slack_auth_spec.rb
+++ spec/integration/slack_auth_spec.rb
@@ -3,13 +3,35 @@ require 'spec_helper'
 describe "Slack OAuth" do
   it "Should have an endpoint available for handling callbacks", :integration do
     uri = [
-      @api_gateway_url,
+      $api_gateway_url,
       "handle_callback?code=fake-code&state=fake-state-id"
     ].join '/'
-    response = Net::HTTP.get_response URI(uri)
+    response = HTTParty.get uri
     expect(response.code.to_i).to eq 200
     expect(response.body).to eq({
       code: 'fake-code'
     }.to_json)
+  end
+
+  context "Slack OAuth - Step 1" do
+    it "Should give me a URL to continue authenticating", :integration do
+      uri = [
+        $api_gateway_url,
+        "begin_authentication"
+      ].join '/'
+      headers = {
+        'x-api-key': ENV["API_KEY"]
+      }
+      response = HTTParty.get(uri, headers)
+      expected_message = "You will need to authenticate into Slack first. \
+To do so, click on or copy/paste \
+the link below, then go to /finish_authentication once done: \
+https://slack.com/oauth/authorize?client_id=#{ENV['SLACK_APP_CLIENT_ID']}&\
+scope=users.profile:read,users.profile:write&\
+redirect_uri=https://#{$api_gateway}/develop/handle_callback&\
+state=fake-state-id"
+      expect(response.code.to_i).to eq 200
+      expect(response.body).to eq(expected_message)
+    end
   end
 end
