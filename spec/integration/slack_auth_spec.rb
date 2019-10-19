30706b14d85baff7533cc6bbd7badb27309cfc7f
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-18 19:50:13 -0500

    Simpler endpoint name

--- spec/integration/slack_auth_spec.rb
+++ spec/integration/slack_auth_spec.rb
@@ -4,7 +4,7 @@ describe "Slack OAuth" do
   it "Should have an endpoint available for handling callbacks", :integration do
     uri = [
       $api_gateway_url,
-      "handle_callback?code=fake-code&state=fake-state-id"
+      "callback?code=fake-code&state=fake-state-id"
     ].join '/'
     response = HTTParty.get uri
     expect(response.code.to_i).to eq 200
