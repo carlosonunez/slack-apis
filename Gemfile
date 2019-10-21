36d9307cf00b0092e3c895e20863d5882ed755c2
Carlos Nunez <carlosonunez@noreply.github.com>
2019-10-20 23:10:41 -0500

    Add capybara and selenium to stack

--- Gemfile
+++ Gemfile
@@ -5,3 +5,8 @@ gem 'rspec-expectations'
 gem 'aws-sdk-dynamodb'
 gem 'logger'
 gem 'httparty'
+group :test do
+  gem 'pry'
+  gem 'selenium-webdriver'
+  gem 'capybara'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require 'rspec'
 require 'slack-api'
 require 'httparty'
 require 'aws-sdk-dynamodb'
+require 'capybara'
+require 'capybara/dsl'
+require 'selenium-webdriver'
 Dir.glob('/app/spec/helpers/**/*.rb') do |file|
   require_relative file
 end
@@ -11,7 +14,12 @@ end
 # in parallel, we need to manually synchronize the state of our tests and
 # manually await certain data becoming available.
 RSpec.configure do |config|
+  config.include Capybara::DSL, :integration => true
   config.before(:all, :integration => true) do
+    ['SELENIUM_HOST', 'SELENIUM_PORT'].each do |required_selenium_env_var|
+      raise "Please set #{required_selenium_env_var}" if ENV[required_selenium_env_var].nil?
+    end
+
     $api_gateway_url = ENV['API_GATEWAY_URL'] || Helpers::Integration::HTTP.get_endpoint
     raise "Please define API_GATEWAY_URL as an environment variable or \
 run 'docker-compose run --rm integration-setup'" \
@@ -20,5 +28,20 @@ run 'docker-compose run --rm integration-setup'" \
     $test_api_key =
       Helpers::Integration::SharedSecrets.read_secret(secret_name: 'api_key') ||
         raise('Please create the "api_key" secret.')
+
+    Capybara.run_server = false
+    Capybara.register_driver :selenium do |app|
+      Capybara::Selenium::Driver.new(
+        app,
+        browser: :remote,
+        url: "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_PORT']}/wd/hub",
+        desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+          "chromeOptions" => {
+            "args" => ['--no-default-browser-check']
+          }
+        )
+      )
+    end
+    Capybara.default_driver = :selenium
   end
 end
