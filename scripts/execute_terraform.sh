ddbe93d7d7f31b8e9c27add5094e0327a08d1801
Carlos Nunez <carlosonunez@noreply.github.com>
2019-11-24 02:45:38 -0600

    Separate Terraform states by environment

--- scripts/execute_terraform.sh
+++ scripts/execute_terraform.sh
@@ -3,13 +3,14 @@ source $(dirname "$0")/helpers/shared_secrets.sh
 TERRAFORM_STATE_S3_KEY="${TERRAFORM_STATE_S3_KEY?Please provide a S3 key to store TF state in.}"
 TERRAFORM_STATE_S3_BUCKET="${TERRAFORM_STATE_S3_BUCKET?Please provide a S3 bucket to store state in.}"
 AWS_REGION="${AWS_REGION?Please provide an AWS region.}"
+ENVIRONMENT="${ENVIRONMENT:-test}"
 
 set -e
 action=$1
 shift
 
 terraform init --backend-config="bucket=${TERRAFORM_STATE_S3_BUCKET}" \
-  --backend-config="key=${TERRAFORM_STATE_S3_KEY}" \
+  --backend-config="key=${TERRAFORM_STATE_S3_KEY}/${ENVIRONMENT}" \
   --backend-config="region=$AWS_REGION" && \
 
 terraform $action $* && \
