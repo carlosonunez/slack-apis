42bdfcdbe01feb78bfe6b04d81ca2ab6264f9828
Carlos Nunez <carlosonunez@noreply.github.com>
2019-11-23 23:19:38 -0600

    Add OAuth specs with changes to match

--- spec/unit/slack_oauth_spec.rb
+++ spec/unit/slack_oauth_spec.rb
@@ -24,19 +24,6 @@ describe "Slack OAuth methods" do
                                                redirect_uri: 'fake')
       access_token = JSON.parse(response.body)['access_token']
       expect(access_token).to eq 'fake-token'
-
-      Helpers::Aws::DynamoDBLocal.drop_tables!
-      fake_event = JSON.parse({
-        requestContext: {
-          identity: {
-            apiKey: 'fake-key'
-          }
-        }
-      }.to_json)
-      SlackAPI::Auth.associate_access_key_to_state_id!(event: fake_event,
-                                                       state_id: 'fake-state-id')
-      expect(SlackAPI::Auth.get_access_key_from_state(state_id: 'fake-state-id'))
-        .to eq 'fake-key'
     end
   end
 end
