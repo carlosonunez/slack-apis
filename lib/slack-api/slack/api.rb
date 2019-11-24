80a64904f05a47924358eea0a32bbc4806b88064
Carlos Nunez <carlosonunez@noreply.github.com>
2019-11-23 23:20:32 -0600

    Make params optional

--- lib/slack-api/slack/api.rb
+++ lib/slack-api/slack/api.rb
@@ -4,14 +4,14 @@ module SlackAPI
   module Slack
     module API
       # Issues a GET request against a Slack API method.
-      def self.get_from(endpoint:, token: nil, params:, content_type: 'application/json')
+      def self.get_from(endpoint:, token: nil, params: {}, content_type: 'application/json')
         params[:token] = token if !token.nil?
         HTTParty.get("https://slack.com/api/#{endpoint}",
                      query: params,
                      headers: { 'Content-Type': content_type })
       end
 
-      def self.post_to(endpoint:, token: nil, params:, body: nil, content_type: 'application/x-www-formencoded')
+      def self.post_to(endpoint:, token: nil, params: {}, body: nil, content_type: 'application/x-www-formencoded')
         params[:token] = token if !token.nil?
         HTTParty.post("https://slack.com/api/#{endpoint}",
                       query: params,
